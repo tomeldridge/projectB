13c3b84db3e707d5e04ed01b0e20b496f9d7a9c0
bententious <bententious@users.noreply.github.com>
2016-11-26 12:09:56 -0800

    added functions
    removed old signup function and added functions for signup, submitanimal, and signing/view animal matches

--- app.js
+++ app.js
@@ -38,37 +38,6 @@ app.get('/signup', function(req,res){
   res.render('signup');
 });
 
-//user signup form handler
-app.post('/signupReq', function(req,res){
-  var rBody = req.body;
-  console.log(rBody);
-
-  var con = mysql.createConnection({
-  host  : 'localhost',
-  user  : 'root',
-  password: 'Password',
-  database: 'petConnectDB'
-  });
-  con.connect(function(err){
-    if(err){
-      console.log('Error Connecting To The DataBase!');
-      return;
-    }
-    console.log('Connection Established To The DataBase');
-  });
-  
-  var context = {};
-  con.query('INSERT INTO profile (fname,lname,city,profileState,phone,email,facebookURL,username,password,isTemp) VALUES (rBody.fName,rBody.lName,rBody.city,rBody.state,rBody.phone,rBody.email,"",rBody.userName,rBody.password,0)', function(err, rows, fields){
-    if(err){
-      return;
-    }
-    context.insert = "pass";
-    
-  });
-
-  con.end(function(err){});
-  res.send(JSON.stringify(context));
-});
 
 //user login page
 app.get('/login', function(req,res){
@@ -127,6 +96,292 @@ app.post('/populateAnimals',function(req,res,next){
 
   con.end(function(err){});  
 });
+
+
+
+//*****************************
+//create new profile (not a temp profile)
+//*****************************
+app.get('/insert', function(req,res,next){
+	
+	var con = mysql.createConnection({
+	  host  : 'localhost',
+	  user  : 'root',
+	  password: 'Password',
+	  database: 'petConnectDB'
+	  });
+	  
+  con.connect(function(err){
+    if(err){
+      console.log('Error Connecting To The DataBase!');
+      return;
+    }
+    console.log('Connection Established To The DataBase');
+  });
+	
+	
+	var newProfileId = null;
+	
+ 	console.log("creating new profile");
+	 con.query("INSERT INTO profile (`fname`, `lname`, `city`, `profileState`, `phone`, `email`, `facebookURL`, `username`, `password`, `isTemp`) VALUES (?,?,?,?,?,?,?,?,?,?)", [req.query.fname, req.query.lname, req.query.city, req.query.state, req.query.phone, req.query.email, 
+	 req.query.facebook, req.query.uname, req.query.pw, 0], function(err, result){
+		if(err){
+		  next(err);
+		  return;
+		}
+
+		//get just-generated userID and store to variable for subsequent db queries
+		newProfileId = result.insertId;
+		
+		//if user selected to host a dog, add to willHost table with user ID variable
+		if(req.query.hostDog == "true")
+		{
+		con.query("INSERT INTO willHost (`userID`, `animalType`) VALUES (?,?)", [newProfileId, "Dog"], function(err, result){
+		if(err){
+			next(err);
+			return;
+		}
+		}); 
+		}	
+
+		//if user selected to host a cat, add to willHost table with user ID variable
+		if(req.query.hostCat == "true")
+		{
+		con.query("INSERT INTO willHost (`userID`, `animalType`) VALUES (?,?)", [newProfileId, "Cat"], function(err, result){
+		if(err){
+			next(err);
+			return;
+		}
+		}); 
+		}
+		
+		//if user selected to host a bird, add to willHost table with user ID variable
+		if(req.query.hostBird == "true")
+		{
+		con.query("INSERT INTO willHost (`userID`, `animalType`) VALUES (?,?)", [newProfileId, "Bird"], function(err, result){
+		if(err){
+			next(err);
+			return;
+		}
+		}); 
+		}
+
+		//if user selected to host a horse, add to willHost table with user ID variable
+		if(req.query.hostHorse == "true")
+		{
+		con.query("INSERT INTO willHost (`userID`, `animalType`) VALUES (?,?)", [newProfileId, "Horse"], function(err, result){
+		if(err){
+			next(err);
+			return;
+		}
+		}); 
+		}		
+
+		//if user selected to help a dog, add to willhelp table with user ID variable
+		if(req.query.helpDog == "true")
+		{
+		con.query("INSERT INTO willHelp (`userID`, `animalType`) VALUES (?,?)", [newProfileId, "Dog"], function(err, result){
+		if(err){
+			next(err);
+			return;
+		}
+		}); 
+		}	
+
+		//if user selected to help a cat, add to willhelp table with user ID variable
+		if(req.query.helpCat == "true")
+		{
+		con.query("INSERT INTO willHelp (`userID`, `animalType`) VALUES (?,?)", [newProfileId, "Cat"], function(err, result){
+		if(err){
+			next(err);
+			return;
+		}
+		}); 
+		}
+		
+		//if user selected to help a bird, add to willhelp table with user ID variable
+		if(req.query.helpBird == "true")
+		{
+		con.query("INSERT INTO willHelp (`userID`, `animalType`) VALUES (?,?)", [newProfileId, "Bird"], function(err, result){
+		if(err){
+			next(err);
+			return;
+		}
+		}); 
+		}
+
+		//if user selected to help a horse, add to willhelp table with user ID variable
+		if(req.query.helpHorse == "true")
+		{
+		con.query("INSERT INTO willHelp (`userID`, `animalType`) VALUES (?,?)", [newProfileId, "Horse"], function(err, result){
+		if(err){
+			next(err);
+			return;
+		}
+		}); 
+		}
+
+		//if user selected to Adopt a dog, add to willAdopt table with user ID variable
+		if(req.query.adoptDog == "true")
+		{
+		con.query("INSERT INTO willAdopt (`userID`, `animalType`) VALUES (?,?)", [newProfileId, "Dog"], function(err, result){
+		if(err){
+			next(err);
+			return;
+		}
+		}); 
+		}	
+
+		//if user selected to Adopt a cat, add to willAdopt table with user ID variable
+		if(req.query.adoptCat == "true")
+		{
+		con.query("INSERT INTO willAdopt (`userID`, `animalType`) VALUES (?,?)", [newProfileId, "Cat"], function(err, result){
+		if(err){
+			next(err);
+			return;
+		}
+		}); 
+		}
+		
+		//if user selected to Adopt a bird, add to willAdopt table with user ID variable
+		if(req.query.adoptBird == "true")
+		{
+		con.query("INSERT INTO willAdopt (`userID`, `animalType`) VALUES (?,?)", [newProfileId, "Bird"], function(err, result){
+		if(err){
+			next(err);
+			return;
+		}
+		}); 
+		}
+
+		//if user selected to Adopt a horse, add to willAdopt table with user ID variable
+		if(req.query.adoptHorse == "true")
+		{
+		con.query("INSERT INTO willAdopt (`userID`, `animalType`) VALUES (?,?)", [newProfileId, "Horse"], function(err, result){
+		if(err){
+			next(err);
+			return;
+		}
+		}); 
+		}			
+	
+	  con.end(function(err){}); 
+		
+	});
+
+
+});
+
+
+//*****************************
+//create new animal in distress
+//*****************************
+app.get('/insertanimal', function(req,res,next){
+	
+	var con = mysql.createConnection({
+	 host  : 'localhost',
+	 user  : 'root',
+	 password: 'Password',
+	 database: 'petConnectDB'
+	 });
+	  
+	con.connect(function(err){
+    if(err){
+      console.log('Error Connecting To The DataBase!');
+      return;
+    }
+    console.log('Connection Established To The DataBase');
+  });
+	
+	
+	
+ 	console.log("creating animal");
+	con.query("INSERT INTO animalInDistress (`animalType`, `description`, `address`, `city`, `animalState`, `finderID`, `status`) VALUES (?,?,?,?,?,?,?)", [req.query.animalType, req.query.description, req.query.address, req.query.city, req.query.state, 
+	 1, "Found"], function(err, result){
+		if(err){
+		  next(err);
+		  return;
+		}
+	
+	con.end(function(err){}); 
+	
+	});
+
+
+});
+
+
+
+
+
+//*****************************
+//login user and product matched animals
+//*****************************
+app.post('/loginuser',function(req,res,next)
+{ 
+
+	var rBody= req.body;
+
+	var con = mysql.createConnection({
+	host  : 'localhost',
+	user  : 'root',
+	password: 'Password',
+	database: 'petConnectDB'
+	});
+	con.connect(function(err)
+	{
+		if(err){
+			console.log('Error Connecting To The DataBase!');
+			return;
+		}
+	console.log('Connection Established To The DataBase');
+	});
+
+	console.log(rBody.uname);
+	console.log(rBody.pw);
+
+
+//get id and password from mySQL that corresponds to the username that was entered
+	con.query('SELECT id, password FROM profile WHERE username=?', [rBody.uname], function(err, rows, fields)
+	{
+   
+   if(err)
+	{
+		next(err);
+		return;
+    }
+    
+	//check if pw is valid
+	if(rBody.pw == rows[0].password)
+	{
+		con.query('SELECT animalInDistress.* FROM animalInDistress JOIN (SELECT DISTINCT willHost.animalType AS animalType FROM willHost JOIN willHelp ON willHost.userID=willHelp.userID JOIN willAdopt ON willAdopt.userID=willHelp.userID WHERE willHost.userID=?) T1 ON T1.animalType=animalInDistress.animalType INNER JOIN (SELECT city FROM profile WHERE profile.id=?) T2 ON T2.city=animalInDistress.city', 
+		[rows[0].id, rows[0].id], function(err, rows, fields)
+		{
+			if(err)
+			{
+				next(err);
+				return;
+			}
+		
+			var context = {};
+			context = JSON.stringify(rows);
+			res.send(context);
+		});
+	}
+		  
+	else 
+	{
+		res.status(401).send({status:401, message: 'unauthorized', type:'internal'}); 
+		console.log("incorrect un pw combination");
+		return;	
+	}
+	
+	con.end(function(err){}); 
+	});	
+	
+});
+
+
 
 
 //basic 404 page
