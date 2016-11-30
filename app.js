0d209a952ca2310b6092f956a01f2a858db1f6b3
Adam Sunderman <sunderad@oregonstate.edu>
2016-11-30 10:17:35 -0800

    adam

--- app.js
+++ app.js
@@ -352,8 +352,8 @@ app.post('/loginuser',function(req,res,next)
     	}
     	console.log(rows);
 	//check if pw is valid
-	console.log(rows.password);
-	if(rBody.pw == rows.password)
+	console.log(rows[0].password);
+	if(rBody.pw == rows[0].password)
 	{
 		con.query('SELECT animalInDistress.* FROM animalInDistress JOIN (SELECT DISTINCT willHost.animalType AS animalType FROM willHost JOIN willHelp ON willHost.userID=willHelp.userID JOIN willAdopt ON willAdopt.userID=willHelp.userID WHERE willHost.userID=?) T1 ON T1.animalType=animalInDistress.animalType INNER JOIN (SELECT city FROM profile WHERE profile.id=?) T2 ON T2.city=animalInDistress.city', 
 		[rows[0].id, rows[0].id], function(err, rows, fields)
