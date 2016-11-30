1c44457c33a80f1cf240f40d9423d4c4259a9db5
Adam Sunderman <sunderad@oregonstate.edu>
2016-11-30 14:52:23 -0800

    adam

--- public/js/login_clientSide.js
+++ public/js/login_clientSide.js
@@ -89,10 +89,7 @@ document.addEventListener("DOMContentLoaded", function(proceed){
 					var uid = makeHiddenInputs("uid",resData.uid);
 					form.appendChild(uid);
 
-					var uname = makeHiddenInputs("uname",resData.uname);
-					form.appendChild(uname);
-
-					var upass = makeHiddenInputs("uid",resData.upass);
+					var upass = makeHiddenInputs("upass",resData.upass);
 					form.appendChild(upass);
 
 					var des = makeHiddenInputs("description", resData.animal[i].description);
